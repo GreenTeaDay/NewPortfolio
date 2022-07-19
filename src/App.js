d21d4603b04f5621b7eea3456b62c6728dc0cbdd
X <63634734+GreenTeaDay@users.noreply.github.com>
2022-07-19 15:23:18 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css';
+import { Navbar} from './components';
+import { Body } from './containers/index.ja';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
     </div>
-  );
-}
+    <Body />
+    </div>
+);
+
 
 export default App;
